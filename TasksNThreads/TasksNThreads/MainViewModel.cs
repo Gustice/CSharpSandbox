aa0195fa64b2933b34e1e2f40c96281b2dc75e7b
Gustice <Jakob.Ger@gmail.com>
2020-10-30 20:38:43 +0100

    feature: Traces added to be able to see processing in the background

--- TasksNThreads/TasksNThreads/MainViewModel.cs
+++ TasksNThreads/TasksNThreads/MainViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using System;
 using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace TasksNThreads
 {
@@ -25,39 +29,72 @@ namespace TasksNThreads
         public DelegateCommand LongAsyncProcessCommand => _longAsyncProcess ??
             (_longAsyncProcess = new DelegateCommand(OnLongAsyncProcess));
 
+        private DelegateCommand _nestedProcessCommand;
+        public DelegateCommand NestedProcessCommand => _nestedProcessCommand ??
+            (_nestedProcessCommand = new DelegateCommand(OnNestedProcess));
+
 
         private string _lastActionLog;
+        private Dispatcher _dispatcher;
+
         public string LastActionLog
         {
             get { return _lastActionLog; }
             set { SetProperty(ref _lastActionLog, value); }
         }
 
+        
         public MainViewModel()
         {
             LogItems.Add("Window loaded triggered");
+
+            _dispatcher = Dispatcher.CurrentDispatcher;
         }
 
         void OnShortSyncProcess()
         {
-            LogItems.Add("OnShortSyncProcess triggered");
+            LogItems.Add("Short SyncProcess triggered");
+            Thread.Sleep(200);
+            // Task.Delay just gets overrun
+            LogItems.Add("Short SyncProcess finished");
         }
 
         void OnLongSyncProcess()
         {
-            LogItems.Add("OnLongSyncProcess triggered");
+            LogItems.Add("Long SyncProcess triggered");
+            Thread.Sleep(5000);
+            // Task.Delay just gets overrun
+            LogItems.Add("Long SyncProcess finished");
         }
 
-        void OnShortAsyncProcess()
+        async void OnShortAsyncProcess()
         {
-            LogItems.Add("OnShortAsyncProcess triggered");
+            LogItems.Add("Short AsyncProcess triggered");
+            await Task.Delay(200);
+            LogItems.Add("Short AsyncProcess finished");
         }
 
-        void OnLongAsyncProcess()
+        async void OnLongAsyncProcess()
         {
-            LogItems.Add("OnLongAsyncProcess triggered");
+            LogItems.Add("Long AsyncProcess triggered");
+            await Task.Delay(5000);
+            LogItems.Add("Long AsyncProcess finished");
         }
 
+        int ProcessNumber = 0;
+        private void OnNestedProcess()
+        {
+            var num = ProcessNumber++; 
+            LogItems.Add("Nested Process triggered");
+
+           Task.Run(() =>
+           {
+               Thread.Sleep(5000);
+               _dispatcher.Invoke(() => LogItems.Add("Process in NestedProcess finished"));
+           });
+
+            LogItems.Add("Nested Process finished");
+        }
 
     }
 }
